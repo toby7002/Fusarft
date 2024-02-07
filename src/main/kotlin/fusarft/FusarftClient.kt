8b4015e36d2ed1790b6a77578f9da5348b89950e
toby7002 <144540995+toby7002@users.noreply.github.com>
2024-02-07 17:57:12 +0700

    Clean codes

--- src/main/kotlin/fusarft/FusarftClient.kt
+++ src/main/kotlin/fusarft/FusarftClient.kt
@@ -2,6 +2,7 @@ package fusarft
 
 import fusarft.block.FBlocks
 import fusarft.fluid.FFluids
+import fusarft.fluid.FluidRender
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry
@@ -12,7 +13,7 @@ import net.minecraft.resources.ResourceLocation
 class FusarftClient : ClientModInitializer {
     override fun onInitializeClient() {
         registerGlassRenderType()
-		registerFluidRender()
+		FluidRender.init()
     }
 
     private fun registerGlassRenderType() {
@@ -20,37 +21,5 @@ class FusarftClient : ClientModInitializer {
         glasses.forEach { glass ->
             BlockRenderLayerMap.INSTANCE.putBlock(glass, RenderType.translucent())
         }
-    }
-
-    private fun registerFluidRender() {
-        FluidRenderHandlerRegistry.INSTANCE.register(
-            FFluids.TRITIUM,
-            FFluids.FLOWING_TRITIUM,
-            SimpleFluidRenderHandler(
-                ResourceLocation("fusarft:block/liquid"),
-                ResourceLocation("fusarft:block/liquid_flow"),
-				0x50fa7b,
-            ),
-        )
-		FluidRenderHandlerRegistry.INSTANCE.register(
-			FFluids.DEUTERIUM,
-			FFluids.FLOWING_DEUTERIUM,
-			SimpleFluidRenderHandler(
-				ResourceLocation("fusarft:block/liquid"),
-				ResourceLocation("fusarft:block/liquid_flow"),
-				0xff5555,
-			),
-		)
-
-        BlockRenderLayerMap.INSTANCE.putFluids(
-            RenderType.translucent(),
-            FFluids.TRITIUM,
-            FFluids.FLOWING_TRITIUM,
-        )
-		BlockRenderLayerMap.INSTANCE.putFluids(
-			RenderType.translucent(),
-			FFluids.DEUTERIUM,
-			FFluids.FLOWING_DEUTERIUM,
-		)
     }
 }
---
+++ src/main/kotlin/fusarft/fluid/FluidRender.kt
@@ -0,0 +1,50 @@
+package fusarft.fluid
+
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.resources.ResourceLocation
+
+class FluidRender {
+	companion object {
+		fun init() {
+			registerDeuterium()
+			registerTritium()
+		}
+
+		private fun registerDeuterium() {
+			FluidRenderHandlerRegistry.INSTANCE.register(
+				FFluids.DEUTERIUM,
+				FFluids.FLOWING_DEUTERIUM,
+				SimpleFluidRenderHandler(
+					ResourceLocation("fusarft:block/liquid"),
+					ResourceLocation("fusarft:block/liquid_flow"),
+					0xff5555,
+				),
+			)
+			BlockRenderLayerMap.INSTANCE.putFluids(
+				RenderType.translucent(),
+				FFluids.DEUTERIUM,
+				FFluids.FLOWING_DEUTERIUM,
+			)
+		}
+
+		private fun registerTritium() {
+			FluidRenderHandlerRegistry.INSTANCE.register(
+				FFluids.TRITIUM,
+				FFluids.FLOWING_TRITIUM,
+				SimpleFluidRenderHandler(
+					ResourceLocation("fusarft:block/liquid"),
+					ResourceLocation("fusarft:block/liquid_flow"),
+					0x50fa7b,
+				),
+			)
+			BlockRenderLayerMap.INSTANCE.putFluids(
+				RenderType.translucent(),
+				FFluids.TRITIUM,
+				FFluids.FLOWING_TRITIUM,
+			)
+		}
+	}
+}
